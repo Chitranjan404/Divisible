a7c592c16812d50aa821c0b9fe0b68bb13ac616c
Reaper-one <74783185+Reaper-one@users.noreply.github.com>
2020-11-20 21:27:31 +0530

    Add files via upload

---
+++ Chitranjan's program-divisible.py
@@ -0,0 +1,9 @@
+a=input("Enter first no.:")
+b=input("Enter second no.:")
+if(a%b==0):
+ print a,'is divisible',b
+else: print a,'is not divisible by',b
+if(b%a==0):
+ print b,'is divisible',a
+else: print b,'is not divisible by',a
+
